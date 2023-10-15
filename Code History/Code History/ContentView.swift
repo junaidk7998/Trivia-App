4a7d6fee79ed25e473322a92bc3b62e77ab646c3
Junaid Khan <junaidk7998@gmail.com>
2023-10-15 09:38:10 -0700

    Refactored Code to make it concise and more readable

---
+++ Code History/Code History/ContentView.swift
@@ -0,0 +1,55 @@
+//
+//  ContentView.swift
+//  Code History
+//
+//  Created by Junaid Khan on 8/6/23.
+//
+
+import SwiftUI
+
+let mainColor = Color(red: 20/255, green: 28/255, blue: 58/255)
+let accentColor = Color(red: 48/255, green: 105/255, blue: 240/255)
+// let white = Color(red: 1, green: 1, blue: 1)
+
+struct ContentView: View {
+    
+    let question = Question(questionText: "What was the first computer bug?", possibleAnswers: ["Ant", "Beetle", "Moth", "Fly"], correctAnswerIndex: 2)
+    
+    @State var mainColor = Color(red: 20/255, green: 28/255, blue: 58/255)
+    //let accentColor = Color(red: 48/255, green: 105/255, blue: 240/255)
+    // let white = Color(red: 1, green: 1, blue: 1)
+    var body: some View {
+        ZStack {
+            mainColor.ignoresSafeArea()
+            VStack {
+                Text("1 / 10")
+                    .font(.callout)
+                    .multilineTextAlignment(.leading)
+                    .padding()
+                Text(question.questionText)
+                    .font(.largeTitle)
+                    .bold()
+                    .multilineTextAlignment(.leading)
+                Spacer()
+                HStack {
+                    ForEach(0..<question.possibleAnswers.count) {
+                        answerIndex in
+                        Button(action: {
+                          print("Tapped on option with the text: \(question.possibleAnswers[answerIndex])")
+                          mainColor = answerIndex == question.correctAnswerIndex ? .green : .red
+                        }) {
+                          ChoiceTextView(choiceText: question.possibleAnswers[answerIndex])
+                        }
+                    }
+                }
+            }
+            .foregroundColor(.white)
+        }
+    }
+}
+
+//struct ContentView_Previews: PreviewProvider {
+   // static var previews: some View {
+      //  ContentView()
+    //}
+//}
---
+++ Code History/Code History/Question.swift
@@ -0,0 +1,14 @@
+//
+//  Question.swift
+//  Code History
+//
+//  Created by Junaid Khan on 10/14/23.
+//
+
+import Foundation
+
+struct Question {
+    let questionText: String
+    let possibleAnswers: [String]
+    let correctAnswerIndex: Int
+}
